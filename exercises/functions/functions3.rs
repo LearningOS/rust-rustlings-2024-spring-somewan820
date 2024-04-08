e88b4beb7244c2d500440652ca99ec51f6ad56ac
2023152027-some <2176978853@qq.com>
2024-04-08 18:41:26 +0800

    update

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -3,14 +3,13 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     call_me();
 }
 
-fn call_me(num: u32) {
-    for i in 0..num {
+fn call_me() {
+    for i in 0..2 {
         println!("Ring! Call number {}", i + 1);
     }
 }
